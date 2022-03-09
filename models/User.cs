a98a2239fdcf777ae913e0506cea255843b952f0
Ryszard Szewczyk <szewczykry@gmail.com>
2022-03-09 10:04:51 +0100

    models

---
+++ models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine4.models
+{
+    public class User
+    {
+        public List<Drink> Drinks { get; set; } = new List<Drink>();
+    }
+}
---
+++ models/VendingMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine4.models
+{
+    public class VendingMachine
+    {
+        public double cash { get; set; }
+        public List<Drink> Drinks { get; set; } = new List<Drink>();
+    }
+}
